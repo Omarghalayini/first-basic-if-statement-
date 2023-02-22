6e85e95bcc8837d0c1d35fc5684ed52331663275
Omarghalayini <42498579+Omarghalayini@users.noreply.github.com>
2023-02-22 06:58:25 +0000

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,47 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int age;
+
+cout << "Enter age: ";
+cin >> age;
+
+if (age < 0){
+
+cout << "Please enter a valid input";
+  }
+  
+  else if  (age >= 18){
+
+    cout << "This person can vote";
+    }
+    
+    else{
+   
+    cout << "This person cannot vote";
+    }
+
+  return 0;
+  }
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
